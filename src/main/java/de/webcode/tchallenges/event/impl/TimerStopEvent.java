7aa472fb8dfafd0fd5963c33a0003b806b041ccb
TheWebcode <89354835+TheWebcode@users.noreply.github.com>
2022-02-19 12:38:19 +0100

    Added Timer Events

---
+++ src/main/java/de/webcode/tchallenges/event/impl/TimerStopEvent.java
@@ -0,0 +1,16 @@
+package de.webcode.tchallenges.event.impl;
+
+import de.webcode.tchallenges.event.Event;
+import de.webcode.tchallenges.utils.ChallengeTimer;
+
+public class TimerStopEvent extends Event {
+    private ChallengeTimer timer;
+
+    public TimerStopEvent(ChallengeTimer timer){
+        this.timer = timer;
+    }
+
+    public ChallengeTimer getTimer() {
+        return timer;
+    }
+}
