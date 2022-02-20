08f588a08c60cceba273456fb67e942a3533a683
TheWebcode <89354835+TheWebcode@users.noreply.github.com>
2022-02-20 12:16:39 +0100

    Added endChallenge Method in API

--- src/main/java/de/webcode/tchallenges/utils/challenge/api/TChallengesAPI.java
+++ src/main/java/de/webcode/tchallenges/utils/challenge/api/TChallengesAPI.java
@@ -1,7 +1,10 @@
 package de.webcode.tchallenges.utils.challenge.api;
 
 import de.webcode.tchallenges.TChallenges;
+import de.webcode.tchallenges.utils.ChallengeTimer;
 import de.webcode.tchallenges.utils.challenge.ChallengeManager;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 
@@ -16,6 +19,19 @@ public class TChallengesAPI {
         ChallengeManager challengeManager = instance.getChallengeManager();
         challengeManager.add(tChallenge);
 
+    }
+
+    public void endChallenge(TChallenge challenge){
+        ChallengeTimer challengeTimer = TChallenges.getInstance().getChallengeTimer();
+        challengeTimer.setRunning(false);
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            player.setGameMode(GameMode.SPECTATOR);
+            player.sendMessage("§e----------------------------------------\n" +
+                    "§7Challenge: "+ challenge.getName() + "\n" +
+                    "§7Die Challenge ist beendet.\n" +
+                    "§7Zeit: " + challengeTimer.getTimeString() +
+                    "\n§e----------------------------------------");
+        });
     }
 
     public static TChallengesAPI getInstance(){
